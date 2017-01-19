c39bd8bc52a7617c25d6a882a79f43a5c2aa1f1b
Edgar <me@edgarlatorre.com.br>
2017-01-19 21:11:50 +0000

    Create RtmApi class for rtm requests

---
+++ lib/slack/rtm_api.rb
@@ -0,0 +1,24 @@
+require 'net/http'
+require 'json'
+
+module Slack
+  class RtmApi
+
+    # Public: Starts a websocket connection with slack
+    #
+    # Examples
+    #
+    #   Slack::RtmApi.start
+    #   # => 'wss://slack-websocket-url'
+    #
+    # Returns a url to connect to slack websocket.
+    def self.start
+      uri = URI('https://slack.com/api/rtm.start')
+      body = JSON.parse(
+        Net::HTTP.post_form(uri, token: ENV['SLACK_TOKEN']).body
+      )
+     
+      body['url']
+    end
+  end
+end
---
+++ tests/slack_test.rb
@@ -0,0 +1,24 @@
+require_relative 'test_helper.rb'
+require './lib/slack/rtm_api.rb'
+require 'webmock/minitest'
+
+class SlackRtmApiTest < Minitest::Test
+  def test_start_returns_websocket_url
+    resp = "{\"url\":\"wss://slack-websocket-url\"}"
+    fake_request(:post, resp)
+    assert Slack::RtmApi.start == 'wss://slack-websocket-url'
+  end
+  
+  def test_start_returns_nil
+    resp = "{\"ok\":false,\"error\":\"invalid_form_data\"}"
+    fake_request(:post, resp)
+    assert Slack::RtmApi.start.nil?
+  end
+
+  private
+
+  def fake_request(method, response)
+    url = 'https://slack.com/api/rtm.start'
+    stub_request(method, url).to_return(body: response)
+  end
+end
