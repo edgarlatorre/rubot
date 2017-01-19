82e26d7a8ad71939597232fd029288978da826f0
Edgar <me@edgarlatorre.com.br>
2017-01-19 23:46:58 +0000

    Change to use event message

--- bot.rb
+++ bot.rb
@@ -1,29 +1,30 @@
-require 'json'
 require 'faye/websocket'
 require 'eventmachine'
 require './lib/slack/rtm_api'
-
+require './lib/slack/event_message'
 
 socket_url = Slack::RtmApi.start
+bot_id = nil
 
-EM.run {
+EM.run do
   ws = Faye::WebSocket::Client.new(socket_url)
 
-  ws.on :open do |event|
-    p [:open]
+  ws.on :open do
+    p 'Opening connection'
   end
 
   ws.on :message do |event|
-    data = JSON.parse(event.data) if event && event.data
-    p [:message, data]
+    message = Slack::EventMessage.new(event)
+    p "Message type: #{message.type} from #{message.user}"
 
-    if data && data['type'] == 'message'
-      p data
+    bot_id = message.user if message.presence_change?
+
+    if message.message?
       ws.send(
         {
           type: 'message',
-          text: "Hi, <@#{data['user']}>",
-          channel: "#{data['channel']}"
+          text: "Hi <@#{message.user}>, I am <@#{bot_id}>",
+          channel: message.channel
         }.to_json
       )
     end
@@ -33,4 +34,4 @@ EM.run {
     p [:close, event.code, event.reason]
     ws = nil
   end
-}
+end
