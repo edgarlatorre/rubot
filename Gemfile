94de3dedc481c84a39ce672d931217d571246ab6
vagrant <vagrant@development>
2017-01-19 08:41:53 +0000

    Initial structure

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'faye-websocket'
+gem 'eventmachine'
+gem 'rake'
+gem 'json'
+
+group :test do
+  gem 'minitest-reporters'
+end
+
+group :development do
+  gem 'rubocop'
+end
---
+++ bot.rb
@@ -0,0 +1,47 @@
+require 'http'
+require 'json'
+require 'faye/websocket'
+require 'eventmachine'
+require './slack'
+
+puts "HEEERE"
+
+Slack::Api::RTM.bla
+
+response = JSON.parse(
+  HTTP.post(
+    'https://slack.com/api/rtm.start',
+    params: {token: ENV['SLACK_TOKEN']}
+  )
+)
+
+socket_url = response['url']
+
+EM.run {
+  ws = Faye::WebSocket::Client.new(socket_url)
+
+  ws.on :open do |event|
+    p [:open]
+  end
+
+  ws.on :message do |event|
+    data = JSON.parse(event.data) if event && event.data
+    p [:message, data]
+
+    if data && data['type'] == 'message'
+      p data
+      ws.send(
+        {
+          type: 'message',
+          text: "Hi, <@#{data['user']}>",
+          channel: "#{data['channel']}"
+        }.to_json
+      )
+    end
+  end
+
+  ws.on :close do |event|
+    p [:close, event.code, event.reason]
+    ws = nil
+  end
+}
