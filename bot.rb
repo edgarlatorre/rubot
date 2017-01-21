aa6637c4fb72f499c7594ccbc7027cfdbe554cd2
Edgar <me@edgarlatorre.com.br>
2017-01-21 15:32:18 +0000

    Use RI class

--- bot.rb
+++ bot.rb
@@ -2,6 +2,7 @@ require 'faye/websocket'
 require 'eventmachine'
 require './lib/slack/rtm_api'
 require './lib/slack/event_message'
+require './lib/ri.rb'
 
 socket_url = Slack::RtmApi.start
 bot_id = nil
@@ -18,12 +19,12 @@ EM.run do
     p "Message type: #{message.type} from #{message.user}"
 
     bot_id = message.user if message.presence_change?
-
+    msg = RI.find('String#eql?')
     if message.message?
       ws.send(
         {
           type: 'message',
-          text: "Hi <@#{message.user}>, I am <@#{bot_id}>",
+          text: "```#{msg}```",
           channel: message.channel
         }.to_json
       )
