838dbfe6054bfb3687132b5ec8af976e3dcf1fc4
Edgar <me@edgarlatorre.com.br>
2017-01-21 20:29:51 +0000

    Create a fake Event

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -2,3 +2,14 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 
 Minitest::Reporters.use!
+
+class EventFake
+  attr_accessor :text, :user, :channel, :type
+
+  def initialize(text, user, channel, type)
+    @text = text
+    @user = user
+    @channel = channel
+    @type = type
+  end
+end
