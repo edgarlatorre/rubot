d3f488dddf31c7a385808278b4637bc5998f89ce
Edgar <me@edgarlatorre.com.br>
2017-01-21 20:39:03 +0000

    Comment test and integer doc

--- tests/ri_test.rb
+++ tests/ri_test.rb
@@ -8,6 +8,14 @@ class RiTest < Minitest::Test
       "  str.eql?(other)   -> true or false\n\n"\
       "------------------------------------------------------------------------------\n\n"\
       "Two strings are equal if they have the same length and content.\n\n\n"
+
+    @int_zero_doc = "= Integer#zero?\n\n"\
+      "(from ruby core)\n"\
+      "=== Implementation from Numeric\n"\
+      "------------------------------------------------------------------------------\n"\
+      "  num.zero?  ->  true or false\n\n"\
+      "------------------------------------------------------------------------------\n\n"\
+      "Returns true if num has a zero value.\n\n\n"
   end
 
   def test_find_return_string_eql_returns_documentation
@@ -19,6 +27,11 @@ class RiTest < Minitest::Test
     text = RI.find('String#eql1?')
     assert text.empty?
   end
+
+  # def test_find_returns_integer_zero_returns_documentation
+  #   text = RI.find('Integer#zero?')
+  #   assert text == @int_zero_doc
+  # end
 
   def test_replace_header_returns_right_header
     text = RI.find('String#eql?')
